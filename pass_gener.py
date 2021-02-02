ddc93660f586ecd40c3ebce25f5929c038325567
Пользователь <@IT-CUBE-NB>
2021-02-02 10:53:31 +0500

    Генерация пароля

---
+++ pass_gener.py
@@ -0,0 +1,19 @@
+import random
+
+
+def generator_password(length: int):
+    password = ''
+    symb = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=ABCDEFGHIJKLMNOPQASTUVWXYZ'
+    for i in range(length):
+        password += random.choice(symb)
+    print(password)
+
+
+def main():
+    a = int(input())
+    b = int(input())
+    for i in range(a):
+        generator_password(b)
+
+
+main()
